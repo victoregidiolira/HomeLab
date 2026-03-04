cca1997d401abcc21d9a649b824395e0e84910f6
victoregidiolira <viegidulira@gmail.com>
2026-03-04 18:12:49 -0300

    chore: Garantir permissão de execução para o script de setup fix: Verificação e instalação da ferramenta 'stress'

--- stress_test.sh
+++ stress_test.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Verificar se o stress está instalado
+if ! command -v stress &> /dev/null; then
+    echo "Erro: A ferramenta 'stress' não está instalada."
+    echo "Instalando via pacman..."
+    sudo pacman -S --noconfirm stress
+fi
+
+# Rodar teste de Estresse
 echo "Iniciando estresse de CPU e Memória por 60s"
 stress --cpu 1 --timeout 30s
 echo "Teste concluído."
